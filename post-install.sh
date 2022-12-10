551a6ecdfc6112e9782800991ed2879763212238
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2022-12-10 22:08:30 +0100

    Change up installation structure and allow a more modular approach.

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,81 @@
 #!/usr/bin/env bash
 
+main() {
+    user="saiba"
+    dotfiles_repo="https://github.com/saiba-tenpura/dotfiles"
+    aur_pkgs="betterlockscreen"
+
+    install_base_dependencies
+    install_additional_software
+    setup_yay $user $aur_pkgs
+    setup_autologin $user
+    setup_dotfiles $user $dotfiles_repo
+    setup_user_configs $user
+}
+
+install_base_dependencies()
+{
+    install_rice_dependencies
+    install_cpu_microcode
+    install_gpu_drivers
+}
+
+install_rice_dependencies() {
+    pacman -S --noconfirm --needed calc dunst feh firefox i3-gaps libnotify neovim nnn papirus-icon-theme picom polkit polybar python-pywal rofi rxvt-unicode ttc-iosevka xorg-xset zathura zathura-pdf-mupdf
+}
+
+install_cpu_microcode() {
+    proc_type=$(lscpu)
+    if [[ $proc_type =~ "GenuineIntel" ]]; then
+        pacman -S --noconfirm --needed intel-ucode
+    elif [[ $proc_type =~ "AuthenticAMD" ]]; then
+        pacman -S --noconfirm --needed amd-ucode
+    fi
+}
+
+install_gpu_drivers() {
+    gpu_type=$(lspci)
+    if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+        pacman -S --noconfirm --needed nvidia nvidia-xconfig
+    elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+        pacman -S --noconfirm --needed xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
+    fi
+
+    pacman -S --noconfirm --needed mesa lib32-mesa
+}
+
+install_additional_software() {
+    install_administration_tools
+    install_wine
+    install_retroarch
+}
+
+install_administration_tools() {
+    pacman -S --noconfirm --needed cups htop openssh
+}
+
+install_wine() {
+    pacman -S --noconfirm --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+}
+
+install_retroarch() {
+    pacman -S --noconfirm --needed retroarch retroarch-assets-xmb
+}
+
+setup_yay() {
+    # Usage: setup_yay <user> <aur_pkgs>
+    temp_sudo="/etc/sudoers.d/01_temp"
+    printf "${1} ALL=(ALL) NOPASSWD: ALL" > $temp_sudo
+
+	su - "${1}" <<-EOF
+	git clone https://aur.archlinux.org/yay.git ~/yay
+	(cd ~/yay; makepkg --noconfirm -si > /dev/null 2>&1; rm -rf ~/yay)
+	yay --noconfirm -S ${2} > /dev/null 2>&1
+	EOF
+
+    rm $temp_sudo
+}
+
 setup_autologin() {
     # Usage: setup_autologin <user>
     mkdir -p /etc/systemd/system/getty@tty1.service.d
@@ -35,55 +111,6 @@ setup_user_configs() {
 	# Install Neovim Plugins
 	nvim --headless +PlugInstall +qall
 	EOF
-}
-
-setup_yay() {
-    # Usage: setup_yay <user> <aur_pkgs>
-    temp_sudo="/etc/sudoers.d/01_temp"
-    printf "${1} ALL=(ALL) NOPASSWD: ALL" > $temp_sudo
-
-	su - "${1}" <<-EOF
-	git clone https://aur.archlinux.org/yay.git ~/yay
-	(cd ~/yay; makepkg --noconfirm -si > /dev/null 2>&1; rm -rf ~/yay)
-	yay --noconfirm -S ${2} > /dev/null 2>&1
-	EOF
-
-    rm $temp_sudo
-}
-
-install_cpu_microcode() {
-    proc_type=$(lscpu)
-    if [[ $proc_type =~ "GenuineIntel" ]]; then
-        pacman -S --noconfirm --needed intel-ucode
-    elif [[ $proc_type =~ "AuthenticAMD" ]]; then
-        pacman -S --noconfirm --needed amd-ucode
-    fi
-}
-
-install_gpu_drivers() {
-    gpu_type=$(lspci)
-    if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
-        pacman -S --noconfirm --needed nvidia nvidia-xconfig
-    elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
-        pacman -S --noconfirm --needed xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
-    fi
-
-    pacman -S --noconfirm mesa lib32-mesa
-}
-
-install_wine() {
-    pacman -S --noconfirm --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
-}
-
-main() {
-    user="saiba"
-    dotfiles_repo="https://github.com/saiba-tenpura/dotfiles"
-    aur_pkgs="betterlockscreen"
-
-    setup_yay $user $aur_pkgs
-    setup_autologin $user
-    setup_dotfiles $user $dotfiles_repo
-    setup_user_configs $user
 }
 
 main
