e6fbfee219601e1bfa30a6802d770381cb5ccb7e
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-05-13 22:28:05 +0200

    Fix evaluation issue for proton installation command.

--- post-install.sh
+++ post-install.sh
@@ -62,19 +62,19 @@ install_administration_tools() {
 
 install_ge_proton() {
 	su - ${user} <<-EOF
-	json=$(curl -s "https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest")
-	[[ $json =~ .*(https.*\.tar\.gz).* ]] && url="${BASH_REMATCH[1]}"
-	filename="${url##*/}"
-	steam_dir=$HOME/.steam/root/compatibilitytools.d
-	mkdir -p "$steam_dir"
-	cd "$steam_dir"
-	curl -sLO "$url"
-	hash=$(curl -Lf ${url//.tar.gz/.sha512sum})
-	if printf '%s' "${hash%% *} ${filename}" | sha512sum -c -; then
-	    tar -xf "$filename"
+	json=\$(curl -s 'https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest')
+	[[ \$json =~ .*(https.*\.tar\.gz).* ]] && url="\${BASH_REMATCH[1]}"
+	filename="\${url##*/}"
+	steam_dir=\$HOME/.steam/root/compatibilitytools.d
+	mkdir -p "\$steam_dir"
+	cd "\$steam_dir"
+	curl -sLO "\$url"
+	hash=\$(curl -Lf \${url//.tar.gz/.sha512sum})
+	if printf '%s' "\${hash%% *} \${filename}" | sha512sum -c -; then
+	    tar -xf "\$filename"
 	fi
 
-	rm "$filename"
+	rm "\$filename"
 	EOF
 }
 
