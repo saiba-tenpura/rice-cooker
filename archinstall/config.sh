5938eac96acf6d7a9ff29f817c24e0641210e38f
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-05-16 21:43:27 +0200

    Move configuration variables to separate config.sh file.

---
+++ archinstall/config.sh
@@ -0,0 +1,20 @@
+# Main user
+user="saiba"
+
+# Additional groups the user is added to
+groups="libvirt"
+
+# Repository of dotfiles which should be installed
+dotfiles_url="https://github.com/saiba-tenpura/dotfiles"
+
+# systemd services to enable by default
+services="bluetooth cronie cups dhcpcd libvirtd"
+
+# Optional user setup steps to include
+# 
+# Available options:
+# ge_proton    - Install the latest GE Proton
+# user_configs - Initialise user setup
+# xautologin   - Setup autologin for Xorg Server
+user_install="ge_proton user_configs xautologin"
+
--- post-install.sh
+++ post-install.sh
@@ -1,45 +1,79 @@
 #!/usr/bin/env bash
 
 main() {
-    config_dir="$1"
+    # Usage: main "setup_name"
+    setup_name="$1"
     script_path="$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
-    packages=$(grep -s -v '^#' "${script_path}/${config_dir}/pkgs.txt")
-    aur_packages=$(grep -s -v '^#' "${script_path}/${config_dir}/aur-pkgs.txt")
-    user="saiba"
-    dotfiles_repo="https://github.com/saiba-tenpura/dotfiles"
+    setup_dir="${script_path}/${setup_name}"
+    if [[ ! -f "${setup_dir}/config.sh" ]]; then
+        printf "Configuration files does not exist."
+        exit 2
+    fi
 
-    [[ -n "${packages}" ]] && install_packages
-    install_aur_packages
-    install_ge_proton
-    setup_yay $user $aur_pkgs
-    setup_additional_services
-    setup_autologin $user
-    setup_dotfiles $user $dotfiles_repo
-    setup_user_configs $user
+    source "${setup_dir}/config.sh" 
+    packages=$(grep -s -v '^#' "${setup_dir}/pkgs.txt")
+    aur_packages=$(grep -s -v '^#' "${setup_dir}/aur-pkgs.txt")
+
+    [[ -n "${packages}" ]] && install_packages $packages
+    install_aur_packages $user $aur_packages
+
+    install_dotfiles $user $dotfiles_url
+    [[ -n "${services}" ]] && install_services $services
+
+    for func in "${user_install}"; do
+        setup_$func $user
+    done
 }
 
 install_packages() {
-    pacman -S --noconfirm --needed $packages
+    # Usage: install_packages "packages"
+    pacman -S --noconfirm --needed $@
 }
 
 install_aur_packages() {
+    # Usage: install_aur_packages "user" "packages"
+    local user=$1 packages="${@:2}" temp_sudo
+
     temp_sudo="/etc/sudoers.d/01_temp"
     echo "${user} ALL=(ALL) NOPASSWD: ALL" > $temp_sudo
 
 	su - "${user}" <<-EOF
-	if ! type yay > /dev/null 2>&1; then
+	if ! type -p yay &>/dev/null; then
 	    git clone https://aur.archlinux.org/yay.git ~/yay
 	    (cd ~/yay; makepkg --noconfirm -si > /dev/null 2>&1; rm -rf ~/yay)
 	fi
 
-	[[ -n "${aur_packages}" ]] && yay -S --noconfirm --needed $aur_packages
+	[[ -n "${packages}" ]] && yay -S --noconfirm --needed $packages
 	EOF
 
     rm $temp_sudo
 }
 
-install_ge_proton() {
-	su - ${user} <<-EOF
+install_dotfiles() {
+    # Usage: setup_dotfiles "user" "url"
+    local user=$1 url=$2
+
+	su - "${user}" <<-EOF
+	curl -sO "${repository/github/raw.githubusercontent}/master/install.sh"
+	chmod 744 install.sh
+	./install.sh ${repository}
+	EOF
+}
+
+install_services() {
+    # Usage: install_services "services"
+    for service in "$@"; do
+        systemctl enable $service.service
+    done
+}
+
+# Optional user setup steps
+
+setup_ge_proton() {
+    # Usage: setup_ge_proton "user"
+    local user=$1
+
+	su - "${user}" <<-EOF
 	json=\$(curl -s 'https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest')
 	[[ \$json =~ .*(https.*\.tar\.gz).* ]] && url="\${BASH_REMATCH[1]}"
 	filename="\${url##*/}"
@@ -56,47 +90,23 @@ install_ge_proton() {
 	EOF
 }
 
-setup_yay() {
-    # Usage: setup_yay <user> <aur_pkgs>
-    temp_sudo="/etc/sudoers.d/01_temp"
-    printf "${1} ALL=(ALL) NOPASSWD: ALL" > $temp_sudo
+setup_xautologin() {
+    # Usage: setup_xautologin "user"
+    local user=$1
 
-	su - "${1}" <<-EOF
-	git clone https://aur.archlinux.org/yay.git ~/yay
-	(cd ~/yay; makepkg --noconfirm -si > /dev/null 2>&1; rm -rf ~/yay)
-	yay --noconfirm -S ${2} > /dev/null 2>&1
-	EOF
-
-    rm $temp_sudo
-}
-
-setup_additional_services() {
-    systemctl enable bluetooth.service
-    systemctl enable libvirtd.service
-}
-
-setup_autologin() {
-    # Usage: setup_autologin <user>
     mkdir -p /etc/systemd/system/getty@tty1.service.d
 	cat <<-EOF > /etc/systemd/system/getty@tty1.service.d/override.conf
 	[Service]
 	ExecStart=
-	ExecStart=-/usr/bin/agetty -o '-p -f -- \\\\u' --noclear -a ${1} %I \$TERM
-	EOF
-}
-
-setup_dotfiles() {
-    # Usage: setup_dotfiles <user> <dotfiles_repo>
-	su - "${1}" <<-EOF
-	curl -sO "${2/github/raw.githubusercontent}/master/install.sh"
-	chmod 744 install.sh
-	./install.sh ${2}
+	ExecStart=-/usr/bin/agetty -o '-p -f -- \\\\u' --noclear -a ${user} %I \$TERM
 	EOF
 }
 
 setup_user_configs() {
-    # Usage: setup_user_configs <user>
-	su - "${1}" <<-EOF
+    # Usage: setup_user_configs "user"
+    local user=$1
+
+	su - "${user}" <<-EOF
 	# Generate Pywal cache for current wallpaper
 	wal -i ~/wallpapers/current
 	
