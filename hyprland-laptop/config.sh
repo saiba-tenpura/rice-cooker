7f0f1634ed576c76e6323daa7b4ebea5cfecf36d
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-05-18 14:06:52 +0200

    Add basic skeleton for hyperland laptop setup.

---
+++ hyprland-laptop/config.sh
@@ -0,0 +1,20 @@
+# Main user
+user="saiba"
+
+# Additional groups the user is added to
+groups=""
+
+# Repository of dotfiles which should be installed
+dotfiles_url="https://github.com/saiba-tenpura/dotfiles"
+
+# systemd services to enable by default
+services="tlp"
+
+# Optional user setup steps to include
+# 
+# Available options:
+# ge_proton    - Install the latest GE Proton
+# user_configs - Initialise user setup
+# xautologin   - Setup autologin for Xorg Server
+user_setup="user_configs"
+
